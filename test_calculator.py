29a0b53e6a4ea946d1274a3ccc5c2a16868227f8
AkellaAdarsh <akella.adarsh@ufl.edu>
2026-04-08 16:02:43 -0400

    #I did Part 3 of my code

--- test_calculator.py
+++ test_calculator.py
@@ -51,6 +51,34 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     ##########################
 
+    def test_multiply(self):
+        self.assertEqual(multiply(4, 5), 20)
+        self.assertEqual(multiply(-3, 6), -18)
+        self.assertEqual(multiply(0, 10), 0)
+
+    def test_divide(self):
+        self.assertEqual(divide(20, 5), 4)
+        self.assertAlmostEqual(divide(7, 2), 3.5)
+        self.assertAlmostEqual(divide(1, 4), 0.25)
+
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            log(0)
+
+        with self.assertRaises(ValueError):
+            log(-10)
+
+    def test_hypotenuse(self):
+        self.assertAlmostEqual(hypotenuse(3, 4), 5.0)
+        self.assertAlmostEqual(hypotenuse(5, 12), 13.0)
+        self.assertAlmostEqual(hypotenuse(8, 15), 17.0)
+
+    def test_sqrt(self):
+        self.assertAlmostEqual(sqrt(25), 5.0)
+        self.assertAlmostEqual(sqrt(2), 1.41421356237, places=5)
+        self.assertAlmostEqual(sqrt(0), 0.0)
+
+
 # Do not touch this
 if __name__ == "__main__":
     unittest.main()
