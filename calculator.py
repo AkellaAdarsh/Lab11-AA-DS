289e7d19c90b82196ba7fb4dcf3c55ac362b15ea
AkellaAdarsh <akella.adarsh@ufl.edu>
2026-04-08 15:54:34 -0400

    #I did Part 3 of my code

--- calculator.py
+++ calculator.py
@@ -1,4 +1,17 @@
 import math
+def square_root(a):
+    try:
+        value = math.sqrt(a)
+    except ValueError:
+        if a < 0:
+            raise ValueError
+    return value
+def hypotenuse(a,b):
+    try:
+        value = math.hypot(a,b)
+    except ValueError:
+        raise ValueError
+    return value
 def add(a, b):
     return a + b
 def subtract(a, b):
